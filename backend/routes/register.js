fd893a51b346b98a5c21928dd2059c13b50d5959
alimirhashimli <alimirhashimli@gmail.com>
2022-07-19 09:38:55 +0200

    adding register router

---
+++ backend/routes/register.js
@@ -0,0 +1,12 @@
+import express from "express";
+import checkValidation from "../validators/checkValidation.js";
+import requiredValues from "../validators/requiredValues.js"
+import registerValidator from "../validators/registerValidators.js"
+import { registerCustomer } from "../controllers/registerController.js"
+
+
+const router = express.Router()
+
+router.post("/", requiredValues(["username", "password", "emailAddress"]), registerValidator(), checkValidation(), registerCustomer);
+
+export default router;
